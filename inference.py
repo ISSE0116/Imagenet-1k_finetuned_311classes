7ca79993fd5cbbf96ab64fe22204bdafa2c3a61e
Your Name <yukizaru.2587@icloud.com>
2022-12-10 16:32:53 +0900

    first commit

---
+++ inference.py
@@ -0,0 +1,58 @@
+################################
+#                              #
+#          Fine-Tuning         #
+#                              #
+################################
+
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import math
+import datetime
+import sys
+from PIL import Image
+import json
+
+path = sys.argv[1]
+input_path = sys.argv[2]
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+PATH = os.path.join('./weight_finetuing_path', path)
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 311)
+model_ft = model_ft.to(device)
+print("学習済み重みをロードしました")
+
+file_path = os.path.join('./input', input_path)
+img = Image.open(file_path)
+#img = cv2.imread(file_path)
+#height, width, channels = img.shape
+
+plt.imshow(img)
+plt.show()
+
+#Load modelpath
+model_ft.load_state_dict(torch.load(PATH, map_location = device))
+model_ft.eval()
+
+#ILSVRC_class_index = json.load(open('./data/imagenet_class_index.json', 'r'))
+#predictor = ILSVRPredictor(ILSVRC_class_index)
+
+print(img.size())
+inputs = img.unsqueeze_(0)
+
+out = model_ft(inputs)
+result = predictor.predict_max(out)
+
+print(result)
---
+++ train.py
@@ -0,0 +1,245 @@
+################################
+#                              #
+#          Fine-Tuning         #
+#           Training           #
+################################
+from __future__ import print_function, division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import math
+import datetime
+
+plt.ion()
+
+#読み取る画像ディレクトリ指定
+data_dir = '../dataset/valval/train'
+
+#####パラメータ設定#####
+batch_size =256 
+num_epochs = 25
+lr = 0.005
+step_size = num_epochs - 5 
+########################
+
+#lossとaccの遷移を記録、グラフに使う
+loss_t=[]
+loss_v=[]
+acc_t=[]
+acc_v=[]
+
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+image_datasets = datasets.ImageFolder(data_dir,data_transforms['train'])
+
+#指定したディレクトリの画像を学習データ、検証データに分割。
+train_size = int(0.8 * len(image_datasets))
+val_size = len(image_datasets) - train_size
+train, val = torch.utils.data.random_split(image_datasets, [train_size, val_size])
+print()
+print("[ABOUT]")
+print("###################################################################")
+print(f"full: {len(image_datasets)} -> train: {len(train)}, validation: {len(val)}")
+print("###################################################################")
+
+print("batch_size: {}, num_epoch: {}, lr(Initial): {}, step_size: {}".format(batch_size, num_epochs, lr, step_size))
+print("###################################################################")
+
+train_loader = torch.utils.data.DataLoader(train, batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val, batch_size, shuffle=True)
+data_loader = {'train':train_loader,'val':val_loader}
+
+train_sizes = len(train)
+val_sizes = len(val)
+dataset_sizes = {'train':train_sizes,'val':val_sizes}
+
+class_names = image_datasets.classes
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+#visualize_trainingdataset
+
+def imshow(inp, title=None):
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0)
+
+#Get a batch of training data
+inputs, classes = next(iter(train))
+
+#Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+#imshow(out, title=[class_names[x] for x in classes])
+
+##########################training models##############################
+def train_model(model, criterin, optimizer, scheduler, num_epochs):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    i = 1
+    
+    global loss_t,loss_v,acc_t,acc_v
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()
+            else:
+               model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in data_loader[phase]:          #dataloaderからdataset呼び出し
+                inputs = inputs.to(device)                     #GPUに転送
+                labels = labels.to(device) 
+                
+                optimizer.zero_grad()
+                
+                with torch.set_grad_enabled(phase == 'train'): #train時のみ勾配の算出をオンにするの意
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+                    i += 1
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)   #lossとbatchサイズとの掛け算
+                running_corrects += torch.sum(preds == labels.data)
+            if(phase == 'train'):
+                epoch_loss_t = running_loss / dataset_sizes['train']
+                epoch_acc_t = running_corrects.double() / dataset_sizes['train']
+                loss_t.append(epoch_loss_t)
+                acc_t.append(epoch_acc_t.item())
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss_t, epoch_acc_t))
+            else:
+                epoch_loss_v = running_loss / dataset_sizes['val']
+                epoch_acc_v = running_corrects.double() / dataset_sizes['val']
+                loss_v.append(epoch_loss_v)
+                acc_v.append(epoch_acc_v.item())
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss_v, epoch_acc_v))
+             
+            if phase == 'val' and epoch_acc_v > best_acc:
+                best_acc = epoch_acc_v
+                best_models_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print('Best val Acc: {:4f}'.format(best_acc))
+    
+    dt_now = datetime.datetime.now()
+    model_path = 'model_path_' + str(dt_now)
+    torch.save(model.state_dict(), os.path.join('weight_finetuing_path', model_path))
+    print()
+    print('!!!!!save_{}!!!!!'.format(model_path))
+    return model
+
+############################################################################################
+
+#visualize predicted model
+
+def visualize_model(model, num_images=6): 
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(data_loader[val]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images// 2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title(f'predicted: {class_names[preds[j]]}')
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+            model.train(mode=was_training)
+
+#finetuning the convert
+
+model_ft = models.resnet18(pretrained=True)
+
+num_ftrs = model_ft.fc.in_features                                                     #modelの定義
+model_ft.fc = nn.Linear(num_ftrs, 311)                                                 #出力層を311classに変更
+model_ft = model_ft.to(device)                                                         #GPUに送信
+criterion = nn.CrossEntropyLoss()                                                      #損失関数定義(分類なのでクロスエントロピー)
+optimizer_ft = optim.SGD(model_ft.parameters(), lr, momentum=0.9, weight_decay=0.0001) #最適化手法(SGD)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size, gamma=0.1)             #スケジューラー
+
+#model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+#visualize_model(model_ft)
+
+train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs)
+
+#plot the result graph
+fig = plt.figure(figsize = (10,5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
+ax1.plot(range(len(loss_t)), loss_t, label="Loss(Train)")
+ax1.plot(range(len(loss_v)), loss_v, label="Loss(Val)")
+ax2.plot(range(len(acc_t)), acc_t, label="Acc(Train)")
+ax2.plot(range(len(acc_v)), acc_v, label="Acc(Val)")
+ax1.legend()
+ax2.legend()
+
+ax1.set_ylim(0,7)
+ax2.set_ylim(0,1)
+ax2.set_yticks([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
+ax2.grid(which = "major", axis = "y", color = "black", alpha = 0.8, linestyle = "--", linewidth = 0.5)
+ax1.set_xlabel("Epochs")
+ax1.set_ylabel("Loss")
+ax2.set_xlabel("Epochs")
+ax2.set_ylabel("Acc")
+plt.savefig('train_result_graph')
+
+print()
+print("!!!!!end_to_plot_graph!!!!!")
+print()
